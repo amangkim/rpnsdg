53a46369bab243eacb755344e0b3e87f35a5d649
Amang Kim <46288213+amangkim@users.noreply.github.com>
2021-04-02 10:54:15 +0800

    Update and rename rnvsdg_demo.m to rpnsdg_demo.m

--- rpnsdg_demo.m
+++ rpnsdg_demo.m
@@ -1,5 +1,5 @@
-% rnvsdg_demo
-% Robust n-person stochastic duel game demo v.0.2
+% rpnsdg_demo
+% Robust Pairwise n-person stochastic duel game demo v.0.2
 %---------------------------------------------------------
 clear all;
 nvsdg_demo_setup
